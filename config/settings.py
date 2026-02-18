15d22970bfe38a85e2a755baeb6bde543235e74a
Justo Arouxet <fairjey1@gmail.com>
2026-02-18 01:56:02 -0300

    FIX: added EMAIL_BACKEND for production

--- config/settings.py
+++ config/settings.py
@@ -144,10 +144,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Configuración de SMTP para envío de correos
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # <--- CORREGIDO: Usamos el backend SMTP real para producción
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465  # <--- CAMBIO: Usamos el puerto SSL
-EMAIL_USE_TLS = False  # <--- CAMBIO: Desactivamos TLS explícito
-EMAIL_USE_SSL = True   # <--- CAMBIO: Activamos SSL directo
+EMAIL_PORT = 465  # <--- CORREGIDO: Usamos el puerto SSL
+EMAIL_USE_TLS = False  # <--- CORREGIDO: Desactivamos TLS explícito
+EMAIL_USE_SSL = True   # <--- CORREGIDO: Activamos SSL directo
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
